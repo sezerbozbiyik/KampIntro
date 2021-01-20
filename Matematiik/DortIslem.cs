19b3002e5d249dd8f66d4a1574cab98736406807
Sezer <Sezer@SEZER-THINKPAD>
2021-01-21 00:06:17 +0300

    Güncelleme denemesi

---
+++ Matematiik/DortIslem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematiik
+{
+    class DortIslem
+    {
+        public void Topla(int sayi1,int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine(sonuc);
+        }
+        public void Cikar(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 - sayi2;
+            Console.WriteLine(sonuc);
+        }
+        public void Carp(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 * sayi2;
+            Console.WriteLine(sonuc);
+        }
+        public void Bol(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 / sayi2;
+            Console.WriteLine(sonuc);
+        }
+    }
+}
---
+++ Matematiik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematiik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 6);
+            dortIslem.Cikar(9,24);
+            dortIslem.Carp(6, 9);
+        }
+    }
+}

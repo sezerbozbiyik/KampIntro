99241d59893b5009af1a642b33f5829ca03e7fd2
Sezer <Sezer@SEZER-THINKPAD>
2021-01-20 23:57:02 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dizilere ekleme yapılmadığından listeler kullanılır.
+            List<string> isimler = new List<string> { "sezer", "hatice", "mehmet" };
+            Console.WriteLine(isimler[0]);
+            isimler.Add("İlker");
+            Console.WriteLine(isimler[3]);
+            Console.WriteLine("------------------------");
+
+            foreach (var isim in isimler)
+            {
+                Console.WriteLine(isim);
+            }
+
+        }
+    }
+}
